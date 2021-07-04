1aeee1e38c5aaf5a9210fda62d3b1d1be249610c
d0rmamuu <neelbeck4@gmail.com>
2021-07-04 13:33:59 +0530

    Co-authored-by: abhij33t-9120 <abhij33t-9120@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-
+const comments= require('./routes/api/comments');
+const xyz=require('./models/Comment');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 4000;
-
-app.use(cors());
 app.use(express.json());
 
 const uri = process.env.DB_URI;
@@ -18,12 +15,45 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-// const exercisesRouter = require('./routes/exercises');
-// const usersRouter = require('./routes/users');
 
-// app.use('/exercises', exercisesRouter);
-// app.use('/users', usersRouter);
+//use routes
+app.use('/api/comments',comments); 
 
+//rotes banao
+app.delete('/delete/:id',(req,res)=>{
+  xyz.findByIdAndDelete(req.params.id,function (err){
+    if(err)
+    console.log(err);
+    else
+    console.log("Deleted");
+  })
+})
+//feedbacks adding
+app.post('/feedbacks',(req,res)=>{
+  const comments=req.body
+  xyz.create(comments,(err,data)=>{
+    if(err){
+      res.status(500).send(err)
+    }
+    else{
+      res.status(201).send(data)
+    }
+  })
+})
+
+//update like part
+app.put('/like',(req,res)=>{
+  const ilike=req.body.like
+  xyz.findByIdAndUpdate(req.body._id,{like:!ilike},(err)=>{
+    if(err)
+    console.log(err)
+    else
+    console.log('like variable updated')
+    res.send('done')
+  })
+})
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
